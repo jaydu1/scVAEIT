04ecfbb5b9b1cec7f5d516f487223efe3a2504cf
jaydu1 <413075930@qq.com>
2024-08-17 14:46:22 -0400

    Update init

--- scVAEIT/__init__.py
+++ scVAEIT/__init__.py
@@ -1,11 +1,10 @@
-__all__ = ['scVAEIT']
+__all__ = ['VAEIT', 'reset_random_seeds']
 
-from scVAEIT.model import VariationalAutoEncoder
-from scVAEIT.utils import reset_random_seeds
 from scVAEIT.VAEIT import VAEIT
+from scVAEIT.utils import reset_random_seeds
 
 __author__ = "Jin-Hong Du"
-__copyright__ = "Copyright 2022, The Cross-Modalilty Transfer Learning Project"
+__copyright__ = "Copyright 2024, The Cross-Modalilty Transfer Learning Project"
 
 __license__ = "MIT"
 __version__ = "1.0.0"
