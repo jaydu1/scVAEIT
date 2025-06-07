544310baa2c0056f41c81103b3e7c547956575db
jaydu1 <413075930@qq.com>
2025-06-07 14:40:36 -0400

    Add version file

--- scVAEIT/__init__.py
+++ scVAEIT/__init__.py
@@ -2,12 +2,12 @@ __all__ = ['VAEIT', 'reset_random_seeds']
 
 from scVAEIT.VAEIT import VAEIT
 from scVAEIT.utils import reset_random_seeds
+from causarray.__about__ import __version__
 
 __author__ = "Jin-Hong Du"
 __copyright__ = "Copyright 2024, The Cross-Modalilty Transfer Learning Project"
 
 __license__ = "MIT"
-__version__ = "1.1.0"
 __maintainer__ = "Jin-Hong Du"
 __email__ = "jinhongd@andrew.cmu.edu"
 __status__ = "Development"
